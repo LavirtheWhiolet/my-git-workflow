c907a441fe0da7d3ee29018f8652979cb9e35781
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2014-09-30 19:36:27 +0600

    Add "config.sh". Remove "--rebase" from pull, rely on Git config.

---
+++ config.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git config --global color.ui true
+git config --global merge.ff false
+git config --global pull.rebase preserve
--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
+#!/bin/sh
+
+# Install "git-*".
 BIN="$HOME/bin"
 ls -1 git-* | xargs -d "\n" -I '{}' sh -c "cp \"{}\" \"$BIN\" && chmod +x \"$BIN/{}\""
+# Install "config.sh".
+exec sh config.sh
